234414c2034b0a818ea56f0b39b217c5857c9611
Unrealplace <liyang84681111819@qq.com>
2016-06-25 08:11:11 +0800

    Initial Commit

---
+++ DreamEXX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DreamEXX
+//
+//  Created by LiYang on 16/6/25.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DreamEXX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DreamEXX
+//
+//  Created by LiYang on 16/6/25.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
